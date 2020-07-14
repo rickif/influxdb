45c130bc3b1f93701e7a42ef33fb7c66e229f91c
Alirie Gray <alirie.gray@gmail.com>
2020-07-14 14:46:31 -0700

    refactor: add org and bucket store and services

--- servicesv2/tenant/error.go
+++ servicesv2/tenant/error.go
@@ -96,3 +96,125 @@ func (e *AggregateError) Err() error {
 	}
 	return nil
 }
+
+var (
+	// ErrOrgNotFound is used when the user is not found.
+	ErrOrgNotFound = &influxdb.Error{
+		Msg:  "organization not found",
+		Code: influxdb.ENotFound,
+	}
+)
+
+// OrgAlreadyExistsError is used when creating a new organization with
+// a name that has already been used. Organization names must be unique.
+func OrgAlreadyExistsError(name string) error {
+	return &influxdb.Error{
+		Code: influxdb.EConflict,
+		Msg:  fmt.Sprintf("organization with name %s already exists", name),
+	}
+}
+
+func OrgNotFoundByName(name string) error {
+	return &influxdb.Error{
+		Code: influxdb.ENotFound,
+		Op:   influxdb.OpFindOrganizations,
+		Msg:  fmt.Sprintf("organization name \"%s\" not found", name),
+	}
+}
+
+// ErrCorruptOrg is used when the user cannot be unmarshalled from the bytes
+// stored in the kv.
+func ErrCorruptOrg(err error) *influxdb.Error {
+	return &influxdb.Error{
+		Code: influxdb.EInternal,
+		Msg:  "user could not be unmarshalled",
+		Err:  err,
+		Op:   "kv/UnmarshalOrg",
+	}
+}
+
+// ErrUnprocessableOrg is used when a org is not able to be processed.
+func ErrUnprocessableOrg(err error) *influxdb.Error {
+	return &influxdb.Error{
+		Code: influxdb.EUnprocessableEntity,
+		Msg:  "user could not be marshalled",
+		Err:  err,
+		Op:   "kv/MarshalOrg",
+	}
+}
+
+// InvalidOrgIDError is used when a service was provided an invalid ID.
+// This is some sort of internal server error.
+func InvalidOrgIDError(err error) *influxdb.Error {
+	return &influxdb.Error{
+		Code: influxdb.EInvalid,
+		Msg:  "org id provided is invalid",
+		Err:  err,
+	}
+}
+
+var (
+	invalidBucketListRequest = &influxdb.Error{
+		Code: influxdb.EInternal,
+		Msg:  "invalid bucket list action, call should be GetBucketByName",
+		Op:   "kv/listBucket",
+	}
+
+	errRenameSystemBucket = &influxdb.Error{
+		Code: influxdb.EInvalid,
+		Msg:  "system buckets cannot be renamed",
+	}
+
+	errDeleteSystemBucket = &influxdb.Error{
+		Code: influxdb.EInvalid,
+		Msg:  "system buckets cannot be deleted",
+	}
+
+	ErrBucketNotFound = &influxdb.Error{
+		Code: influxdb.ENotFound,
+		Msg:  "bucket not found",
+	}
+
+	ErrBucketNameNotUnique = &influxdb.Error{
+		Code: influxdb.EConflict,
+		Msg:  "bucket name is not unique",
+	}
+)
+
+// ErrBucketNotFoundByName is used when the user is not found.
+func ErrBucketNotFoundByName(n string) *influxdb.Error {
+	return &influxdb.Error{
+		Msg:  fmt.Sprintf("bucket %q not found", n),
+		Code: influxdb.ENotFound,
+	}
+}
+
+// ErrCorruptBucket is used when the user cannot be unmarshalled from the bytes
+// stored in the kv.
+func ErrCorruptBucket(err error) *influxdb.Error {
+	return &influxdb.Error{
+		Code: influxdb.EInternal,
+		Msg:  "user could not be unmarshalled",
+		Err:  err,
+		Op:   "kv/UnmarshalBucket",
+	}
+}
+
+// BucketAlreadyExistsError is used when attempting to create a user with a name
+// that already exists.
+func BucketAlreadyExistsError(n string) *influxdb.Error {
+	return &influxdb.Error{
+		Code: influxdb.EConflict,
+		Msg:  fmt.Sprintf("bucket with name %s already exists", n),
+	}
+}
+
+// ErrUnprocessableBucket is used when a org is not able to be processed.
+func ErrUnprocessableBucket(err error) *influxdb.Error {
+	return &influxdb.Error{
+		Code: influxdb.EUnprocessableEntity,
+		Msg:  "user could not be marshalled",
+		Err:  err,
+		Op:   "kv/MarshalBucket",
+	}
+}
